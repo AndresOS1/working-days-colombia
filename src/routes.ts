7dac2a1b748735a15405b53f9077488a4f9fdf34
AndresOS1 <ordonezandres202@gmail.com>
2025-09-17 16:32:14 -0500

    feat: add main route that responds with "Hello World"

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/", (request: Request, response: Response) => { 
+  response.status(200).send("Hello World");
+}); 
+
+export default router;
