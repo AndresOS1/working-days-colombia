0ddd291d24e63c36651f63f21152a6bb22ec113d
AndresOS1 <ordonezandres202@gmail.com>
2025-09-18 15:52:03 -0500

    refactor: update ESLint configuration and improve linting scripts in package.json

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    ignores: ["eslint.config.ts"],
+    ignores: ["eslint.config.ts", "dist/**"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
