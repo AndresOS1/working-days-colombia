86eb9d07c2d9572a5c3528b49a34085584b53187
AndresOS1 <ordonezandres202@gmail.com>
2025-09-18 02:03:59 -0500

    chore: update devDependencies for linting and TypeScript support
    - Added @eslint/js and eslint for improved linting capabilities.
    - Included globals and jiti for enhanced development environment.
    - Updated typescript-eslint to the latest version for better TypeScript integration.

---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+// eslint.config.ts
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+]);
